3d701165098eea73bd9b9631b769058ac6dfb34b
bweegee <bw33g33@gmail.com>
2019-05-04 07:59:43 -0600

    problem set 0

--- ps0.py
+++ ps0.py
@@ -3,10 +3,10 @@
 # 3. Prints out the user’s first and last names in that order. 
 
 def hello_world():
-    print('What is your first name?')
-    first_name = input()
     print('What is your last name?')
     last_name = input()
+    print('What is your first name?')
+    first_name = input()
     print('Nice to meet you, ' + first_name + ' ' + last_name)
 
 hello_world()
